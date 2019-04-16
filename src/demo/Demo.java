0e2d544c3c5ce353d781ba371eeb27fbc3341d9a
AndreasPatsimas <45208441+AndreasPatsimas@users.noreply.github.com>
2019-04-16 11:22:07 +0300

    a simple change

--- src/demo/Demo.java
+++ src/demo/Demo.java
@@ -17,6 +17,7 @@ public class Demo {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hello git.");
+        int x = 3;
     }
     
 }
